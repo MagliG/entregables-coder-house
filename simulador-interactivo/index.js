9fae8ec45dfd99d7079ce2467dfea8294232a1af
Matías Iglesias <miglesias@campus.ungs.edu.ar>
2021-12-07 20:35:24 -0300

    Se sube simulador interactivo

---
+++ simulador-interactivo/index.js
@@ -0,0 +1,32 @@
+const restauranteUno = "La Bricciola"
+const restauranteDos = "Gusto Ibérico"
+
+let cantidadMesasDisponiblesUno = 12
+let cantidadMesasDisponiblesDos = 0
+
+const restauranteElegido = parseInt(prompt("Elegir el restaurante para realizar su reserva: \n"+ "1. " + restauranteUno + "\n" + "2. " + restauranteDos))
+
+
+function verificarCuposMesa(cantidadMesas){
+    if( cantidadMesas > 0 ){
+       let nombreCliente =  prompt("El restaurante tiene mesas disponibles. Ingrese su nombre de reserva: ")
+       console.log(nombreCliente + " su reserva fue realizada con éxito.")
+       cantidadMesas--
+       console.log("Quedan " + cantidadMesas + " mesas disponibles.")
+    } else {
+        alert("No hay mesas disponibles para el restaurante seleccionado.")
+    }
+}
+
+if( restauranteElegido != null){
+    switch(restauranteElegido){
+        case 1: {
+            verificarCuposMesa(cantidadMesasDisponiblesUno)
+            break;
+        }
+        case 2: {
+            verificarCuposMesa(cantidadMesasDisponiblesDos)
+            break;
+        }
+    }
+}
