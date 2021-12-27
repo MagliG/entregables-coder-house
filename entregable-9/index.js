c149f6d6cb8682c653c3c7bde512d38355adc63e
Matías Iglesias <miglesias@campus.ungs.edu.ar>
2021-12-27 02:59:51 -0300

    Se agrega entregable 9

---
+++ entregable-9/index.js
@@ -0,0 +1,52 @@
+class Reserva {
+  constructor (nombreCliente, menu, cantidadPersonas) {
+    ;(this.nombreCliente = nombreCliente),
+      (this.menu = menu),
+      (this.cantidadPersonas = cantidadPersonas)
+  }
+
+  cargarReserva () {
+    if (this.nombreCliente !== '') {
+      localStorage.setItem(this.nombreCliente, JSON.stringify(this))
+      return 'Reserva cargada'
+    } else {
+      return 'ERROR: No se pudo crear el usuario'
+    }
+  }
+}
+
+function definirEventos () {
+  function mostrarReservas () {
+    const listaReservas = document.createElement('ul')
+    const reservas = document.getElementById('reservas')
+    for (let i = 0; i < localStorage.length; i++) {
+      const llave = localStorage.key(i)
+      let reserva = JSON.parse(localStorage.getItem(llave))
+      let plantilla = `<p>Nombre del cliente: ${reserva.nombreCliente}</p>
+      <p>Menú: ${reserva.menu}</p>
+      <p>Cantidad de personas: ${reserva.cantidadPersonas}</p>
+      <hr>`
+      let itemReserva = document.createElement('li')
+      itemReserva.innerHTML = plantilla
+      listaReservas.appendChild(itemReserva)
+    }
+    reservas.appendChild(listaReservas)
+  }
+  const cargarReserva = document.getElementById('cargarReserva')
+  cargarReserva.onclick = e => {
+    e.preventDefault()
+    const nuevaReserva = new Reserva(
+      document.getElementById('nombreCliente').value,
+      document.getElementById('menu').value,
+      document.getElementById('cantPersonas').value
+    )
+    console.log(nuevaReserva)
+    const respuesta = nuevaReserva.cargarReserva()
+    if (respuesta === 'Reserva cargada') {
+      alert('Reserva realizada con exito')
+    } else {
+      alert('ERROR')
+    }
+    mostrarReservas()
+  }
+}
